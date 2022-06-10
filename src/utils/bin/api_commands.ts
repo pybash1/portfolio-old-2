5adc9b7033c91d5e6e028d4f0757e24077838fcc
pybash1 <example@example.com>
2022-06-10 12:49:58 +0530

    Change project layout to grid

--- src/utils/bin/api_commands.ts
+++ src/utils/bin/api_commands.ts
@@ -6,57 +6,38 @@ import { getWeather } from '../api';
 
 export const projects = async (args: string[]): Promise<string> => {
   return `
-╭──────────────────────────────────────────────────────── <a href="https://github.com/pybash1/befit">pybash1/befit</a> ─────────────────────────────────────────────────────────╮
-│ ╔════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╗ │
-│ ║                                                  <a href="https://befit-tracker.vercel.app">BeFit - Fitness Tracker</a>                                                   ║ │
-│ ╚════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╝ │
-│                                                                                                                                │
-│ BeFit is an all in one modern fitness tracker. It logs ur workouts, exercises, and much more, while providing you with an      │
-│ extensive list of 800+ exercises (with detailed instructions)! It can also help you meditate and show your fitness score.      │
-╰────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────╯
+╭────────────────────── pybash1/befit ───────────────────────╮ ╭────────────────────── rrpm-org/rrpm ───────────────────────╮
+│ ╔════════════════════════════════════════════════════════╗ │ │ ╔════════════════════════════════════════════════════════╗ │
+│ ║               BeFit - Fitness Tracker                  ║ │ │ ║    RRPM - Remote Repository and Project Management     ║ │
+│ ╚════════════════════════════════════════════════════════╝ │ │ ╚════════════════════════════════════════════════════════╝ │
+│                                                            │ │                                                            │
+│ BeFit is an all in one modern fitness tracker. It logs     │ │ RRPM is a command line tool to track, clone, and organize  │
+│ workouts, exercises, and much more, while providing you    │ │ remote repositories and your projects. It also has an      │
+│ with and extensive list of 800+ exercises (with detailed   │ │ universal tool to generate different project structures    │
+│ instructions)! It can also help you meditate and show your │ │ platform independently! Which leverages the  use of other  │
+│ fitness score.                                             │ │ command line tools.                                        │
+╰────────────────────────────────────────────────────────────╯ ╰────────────────────────────────────────────────────────────╯
 
-╭──────────────────────────────────────────────────────── <a href="https://github.com/rrpm-org/rrpm">rrpm-org/rrpm</a> ─────────────────────────────────────────────────────────╮
-│ ╔════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╗ │
-│ ║                                        RRPM - Remote Repository and Project Management                                     ║ │
-│ ╚════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╝ │
-│                                                                                                                                │
-│ RRPM is a command line tool to track, clone, and organize remote repositories and your projects. It also has an universal      │
-│ tool to generate different project structures platform independently! Which leverages the  use of other command line tools.    │
-╰────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────╯
+╭──────────────── CutCode-org/CutCode-svelte ────────────────╮ ╭──────────────────── pastegram/backend ─────────────────────╮
+│ ╔════════════════════════════════════════════════════════╗ │ │ ╔════════════════════════════════════════════════════════╗ │
+│ ║                    CutCode Website                     ║ │ │ ║                  Pastegram - Backend                   ║ │
+│ ╚════════════════════════════════════════════════════════╝ │ │ ╚════════════════════════════════════════════════════════╝ │
+│                                                            │ │                                                            │
+│ CutCode's official website, rewritten in Svelte which      │ │ Pastegram's backend migrated and rewritten in Python       │
+│ displays binaries based on client's operating system.      │ │ (FastAPI), with Deta as a Database. It is a GitHub Gists   │
+│                                                            │ │ like pasting service.                                      │
+╰────────────────────────────────────────────────────────────╯ ╰────────────────────────────────────────────────────────────╯
 
-╭────────────────────────────────────────────────── <a href="https://github.com/CutCode-org/CutCode-svelte">CutCode-org/CutCode-svelte</a> ──────────────────────────────────────────────────╮
-│ ╔════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╗ │
-│ ║                                                       CutCode Website                                                      ║ │
-│ ╚════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╝ │
-│                                                                                                                                │
-│         CutCode's official website, rewritten in Svelte which displays binaries based on client's operating system.            │
-╰────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────╯
-
-╭───────────────────────────────────────────────────── <a href="https://github.com/pastegram/backend">pastegram/backend</a> ────────────────────────────────────────────────────────╮
-│ ╔════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╗ │
-│ ║                                                   Pastegram - Backend                                                      ║ │
-│ ╚════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╝ │
-│                                                                                                                                │
-│ Pastegram's backend migrated and rewritten in Python(FastAPI), with Deta as a Database. It is a GitHub Gists like pasting      │
-│ service.                                                                                                                       │
-╰────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────╯
-
-╭────────────────────────────────────────────────── <a href="https://github.com/Cotidie-project/backend">Cotidie-project/backend</a> ─────────────────────────────────────────────────────╮
-│ ╔════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╗ │
-│ ║                                                   Cotidie - Backend                                                        ║ │
-│ ╚════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╝ │
-│                                                                                                                                │
-│                       Cotidie is a daily planner, that plans your day/week for you from a list of tasks.                       │
-╰────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────╯
-
-╭───────────────────────────────────────────────────── <a href="https://github.com/pybash1/openauth">pybash1/openauth</a> ─────────────────────────────────────────────────────────╮
-│ ╔════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╗ │
-│ ║                                    OpenAuth - The open source authentication library                                       ║ │
-│ ╚════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╝ │
-│                                                                                                                                │
-│ OpenAuth is a library that does authentication for you, but on your resources. It has magic links, but on your mail server!    │
-│ Paswordless OAuth, but your database! And expanding to much more!                                                              │
-╰────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────╯
+╭────────────────── Cotidie-project/backend ─────────────────╮ ╭───────────────────── pybash1/openauth ─────────────────────╮
+│ ╔════════════════════════════════════════════════════════╗ │ │ ╔════════════════════════════════════════════════════════╗ │
+│ ║                    Cotidie - Backend                   ║ │ │ ║   OpenAuth - The open source authentication library    ║ │
+│ ╚════════════════════════════════════════════════════════╝ │ │ ╚════════════════════════════════════════════════════════╝ │
+│                                                            │ │                                                            │
+│ Cotidie is a daily planner, that plans your day/week for   │ │ OpenAuth is a library that does authentication for you,    │
+│ you from a list of tasks.                                  │ │ but on your resources. It has magic links, but on your     │
+│                                                            │ │ mail server! Paswordless OAuth, but your database! And     │
+│                                                            │ │ expanding to much more!                                    │
+╰────────────────────────────────────────────────────────────╯ ╰────────────────────────────────────────────────────────────╯
 `;
 };
 
